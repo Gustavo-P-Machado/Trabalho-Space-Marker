de4ded6a918b925d3a77090327d401e02a8fab00
Felipe Molz <felipeborbamolz@gmail.com>
2024-06-06 16:52:17 -0300

    Versão Atualizada

--- main.py
+++ main.py
@@ -1,72 +1,78 @@
-import pygame, tkinter
-
-#Colocar uma imagem de fundo com algumas estrelinhas uiui
-#criar 3 opções: Salvar, Carregar e Excluir as marcações
-    #Fazer essas opções funcionarem
-#Quando clicar em qualquer lugar da imagem abrir uma caixa de diálogo pedindo par dar um nome a esse ponto
-#Mostrar na tela o luagr em que foi clicado, junto das coordenadas da tela
-#Criar automaticamente uma linha ligando as estrelas, que mostra as distancia entre cada estrela
-#Criar um sistema de salvamento automárico quando o usuário sair do programa
-    #Fazer com que a tecla 'esc'  feche o programa, bem como o botão 'x'
+import pygame
+from tkinter import simpledialog
 
 
-pygame.init() #inicia a biblioteca "pygame"
-tamanho = (1080, 720) #tamanho da tela
-clock = pygame.time.Clock() #define o fps 
-tela = pygame.display.set_mode(tamanho) #cria o display da tela
+# Inicaliza o Pygame
+pygame.init() 
+
+# Configura a janela do Pygame
+tamanho = (1080, 720)
+tela = pygame.display.set_mode(tamanho) 
 imgicone  = pygame.image.load("assets/icone.png")
 icone = pygame.transform.scale(imgicone, (32, 32))
-pygame.display.set_icon(icone)
-pygame.display.set_caption('Space Marker') #cria o título da tela
-
 picture = pygame.image.load('assets/imgEspaco.jpg')
 bg = pygame.transform.scale(picture, (1080, 720))
 
-posX = 0
-posY = 0
+# Configura o icone e título do Pygame
+pygame.display.set_icon(icone)
+pygame.display.set_caption('Space Marker')
 
-branco = (255, 255, 255)
+# Funções
+def linhas():
+    for coord in range(1, len(estrelas)):
+        pygame.draw.aaline(tela, branco, estrelas[coord - 1], estrelas[coord], 3)
 
 def desenhar():
     for num in range(len(estrelas)):
         pygame.draw.circle(tela, branco, estrelas[num], 5, 0)
 
-def linhas():
-    #estrelasUltimo = list(estrelas.keys())[-2]
-    #pygame.draw.aaline(tela, branco, (posStart), (estrelas[estrelasUltimo]), 2)
-    for coord in range(1, len(estrelas)):
-        pygame.draw.aaline(tela, branco, estrelas[coord - 1], estrelas[coord], 3)
-
-    
+# Variáveis Loop
 posDict = 0
-estrelas = {
-
-}
-
 posStart = 0, 0
+branco = (255,255,255)
+estrelas = {}
+tela.fill(branco)
+tela.blit(bg, (0, 0))
 
+posX = 0
+posY = 0
 
-while True: #mantém a tela aberta até que a tela seja fechada
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            quit()
-        if evento.type == pygame.MOUSEBUTTONDOWN:
-            posX, posY = evento.pos
+# Loop principal do jogo
+jogando = True
+while jogando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogando = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            posX, posY = event.pos
             posStart = (posX, posY)
             estrelas[posDict] = posX, posY
             posDict += 1
-            
+        
 
-    tela.fill(branco)
-    tela.blit(bg, (0, 0))
-            
-    if len(estrelas) >= 2:
-        linhas()
+    #if len(estrelas) >= 2:
+    linhas()
         
     desenhar()
 
+
+    # Atualiza a tela    
+    pygame.display.update() 
+
+# Finaliza o Pygame
+pygame.quit()
+
     
 
-    pygame.display.update() #diz pra atualizar a tela
-    clock.tick(60) #diz que a tela vai ter 60 fps
+
+    
+
+    
+
+    
+
+    
+
+
+
 
